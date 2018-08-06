b5b753e6836686742aef6d1210936788e2e5ad45
helenosheaa <helenosheaa@gmail.com>
2018-08-06 15:21:56 +0100

    pub sub helper added and input view logging out

--- src/views/input_view.js
+++ src/views/input_view.js
@@ -1,9 +1,16 @@
+const PubSub = require('../helpers/pub_sub.js');
+
 const InputView = function() {
 
 }
 
 InputView.prototype.bindEvents = function () {
-
+  const input = document.querySelector('#text');
+  input.addEventListener('input', (evt) => {
+    const inputtedText = evt.target.value;
+    // console.log('Inputted text:', inputtedText);
+    PubSub.publish('InputView:text-inputted', inputtedText);
+  })
 };
 
 module.exports = InputView;
